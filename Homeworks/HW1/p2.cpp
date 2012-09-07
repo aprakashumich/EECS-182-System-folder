70815a8e6a721d29eeca06f6f7274d694edcd8b8
Atul Prakash <aprakash@umich.edu>
2012-09-07 00:12:17 -0400

    updates

--- Homeworks/HW1/p2.cpp
+++ Homeworks/HW1/p2.cpp
@@ -7,27 +7,43 @@ using namespace::std;
 // by the compiler. Also anything after a // is ignored by the compiler
 
 /*  
- * Task 1: Add your name below
+ * Task 0: Run the program as is by compiling it and running it. It should print "Hello World".
+ * The % indicates the terminal prompt. You need to open a Terminal, and type "cd Homeworks/HW1"
+ * before running these commands to change to the Homeworks/HW1 folder.
+
+      % g++ -o p2.out p2.cpp
+      % p2.out
+
+ 
+
+ *  Task 1:  First remove the p2.out file.
+	% rm p2.out
+   
+    Now, reRun the program by compiling it using an alternative method of using Makefiles. 
+      % make
+      % p2.out
+
+    Explanation: The first command above runs a program called "make". "make" uses the compilation rules in a file named "Makefile" to build p2.out from p2.cpp.
+
+ * The remaining tasks ask you to make changes to this file, recompile it, run it to see that the
+ * changes do the right thing, and then submit the modified program..
+
+ * Task 3: Add your name below
  * Name: xxxxxxxxxxxxxxxxxxx
- * Task 2: Acknowledge any significant help received below. If no significant
+
+ * Task 4: Acknowledge any significant help received below. If no significant
  * help received, indicate so. Feel free to add additional lines.
  * Acknowledgements: xxxxxxxxxxxxxxx
  */
 
 
 int main() {
-  // Task 3:
-  // Replace "Hello World" by "Hello Prof. Prakash, Vidal, Lars, and DJ".
-  // Compile and run your program. To compile, do one of the following:
-  // % g++ -g -o hello.out hello.cpp
-  // % hello.out
-  // or
-  // % make
-  // % hello.out
-  // (The latter works using a "Makefile". It contains rules for compiling
-  // your code.)
+  /* Task 5:
+    Replace "Hello World" by "Go Blue!".
+    Compile and run your program.
+   Submit the revised program, p2.cpp, and a screenshot
+   that shows the Terminal window with the commands you typed and the result.  
+   */
 
-  // Submit the revised program and a screenshot
-  // of the running program.
   cout << "Hello World" << endl;
 }
