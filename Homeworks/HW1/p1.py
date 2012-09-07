98523dc54936d58fb4865aa71bc999237d8cae6f
Atul Prakash <aprakash@umich.edu>
2012-09-06 21:49:37 -0400

    added HW1

---
+++ Homeworks/HW1/p1.py
@@ -0,0 +1,15 @@
+# Python. Problem 1.
+# Python uses the pound sign for comments. Anything after the pound symbol
+# is ignored by the Python interpreter.
+
+# Task 1: Put your name on the line below
+# Name: xxxxxxxxxxxx
+
+# Task 2: Run the program as follows from a terminal window. You should get Hello World 
+# printed out. 
+# % python p1.py
+
+# Task 3: Modify the following line using emacs so that it prints out "Go Wolverines!" instead
+# of "Hello World"
+
+print "Hello World"
