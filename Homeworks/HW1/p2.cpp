ffa937f854de5617b491008335f3a342956eae5c
Atul Prakash <aprakash@umich.edu>
2012-09-06 21:39:57 -0400

    updates

---
+++ Homeworks/HW1/p2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace::std;
+
+/* HW1: Problem 2 */
+
+// Anything between /* and */ is a comment in C++ and ignored by
+// by the compiler. Also anything after a // is ignored by the compiler
+
+/*  
+ * Task 1: Add your name below
+ * Name: xxxxxxxxxxxxxxxxxxx
+ * Task 2: Acknowledge any significant help received below. If no significant
+ * help received, indicate so. Feel free to add additional lines.
+ * Acknowledgements: xxxxxxxxxxxxxxx
+ */
+
+
+int main() {
+  // Task 3:
+  // Replace "Hello World" by "Hello Prof. Prakash, Vidal, Lars, and DJ".
+  // Compile and run your program. To compile, do one of the following:
+  // % g++ -g -o hello.out hello.cpp
+  // % hello.out
+  // or
+  // % make
+  // % hello.out
+  // (The latter works using a "Makefile". It contains rules for compiling
+  // your code.)
+
+  // Submit the revised program and a screenshot
+  // of the running program.
+  cout << "Hello World";
+}
