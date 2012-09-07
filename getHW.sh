9e50720f5b4493c68028c5f3349ddbfb7fbee22d
Atul Prakash <aprakash@umich.edu>
2012-09-07 00:02:56 -0400

    updates to getHW.sh

--- getHW.sh
+++ getHW.sh
@@ -10,3 +10,13 @@ do
     fi
     cp -i -r "$hwfolder"/* /home/me/Homeworks/"$hwfolder"/     
 done
+
+# Make another copy
+for hwfolder in *
+do
+    if [ ! -d /home/me/Homeworks_OriginalVersion/hwfolder" ]; then
+        echo making directory /home/me/Homeworks_OriginalVersion/"$hwfolder"
+        mkdir -p /home/me/Homeworks_OriginalVersion/"$hwfolder"
+    fi
+    cp -f -r "$hwfolder"/* /home/me/Homeworks/"$hwfolder"/     
+done
