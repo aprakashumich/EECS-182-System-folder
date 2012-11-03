53624a70b0f7fc30ab3e6284549598b46c6fbe7f
Atul Prakash <aprakash@umich.edu>
2012-11-03 18:49:54 -0400

    updates

--- Homeworks/HW7/grademyprogram.sh
+++ Homeworks/HW7/grademyprogram.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-echo "Grading the C++ part of the assignment"
+echo "Points received:\t0" > GRADERESULT_loopshw.txt
+echo "Grading the C++ part of the assignment (out of 15 points)"
+echo "If you get compiler errors, consider your score to be 0"
 g++ -o grade.out grade_loopshw.o loopshw.cpp
 ./grade.out
 
-echo "Grading the Python part of the assignment"
+echo "Grading the Python part of the assignment (out of 10 points)"
 python grade.pyc
 
 echo "You should add the above two scores for your total autograder score";
