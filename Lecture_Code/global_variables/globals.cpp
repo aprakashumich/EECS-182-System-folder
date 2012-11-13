523262da0ceaa9670f9b8a2817b9353b7c367fff
Atul Prakash <aprakash@umich.edu>
2012-11-12 21:36:29 -0500

    updates to global.cpp

--- Lecture_Code/global_variables/globals.cpp
+++ Lecture_Code/global_variables/globals.cpp
@@ -6,31 +6,32 @@ int x = 0;              // Declare a global variable x
 
 void f1() {
     cout << x << endl;  // prints the value of global x.
-    x = x + 1;          // x refers to global x
+    x = x + 1;          // increments global x
 }
 
 void f2() {
-    int x;              // local x
-    x = 3;              // x refers to local x
+    int x;              // local x. Masks global x.
+    x = 3;              // Sets local x
 }
 
-void f3(int x) {
-    x = 4;               // x refers to parameter x
+void f3(int x) {         // Parameter x masks global x.
+    x = 4;               // x refers to parameter x.
 }
 
-void f4(int &x) {
-    x = 5;               // x refers to parameter x (alias)
+void f4(int &x) {        // Alias parameter x masks global x.
+    x = 5;               // x refers to aliased parameter x 
 }
 
 int main() {
-    x = 1;               // refers to global x
+    x = 1;               // Sets global x
     f1();
     cout << "global x after f1() = " << x << endl;
     f2();
     cout << "global x after f2() = " << x << endl;
     f3(x);
     cout << "global x after f3() = " << x << endl;
-    f4(x);
+    int y = 1;
+    f4(y);
     cout << "global x after f4() = " << x << endl;
     if (true) {
         int x;             // local x. Masks global x
