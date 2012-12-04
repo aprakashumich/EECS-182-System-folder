241b13d3132b119ac8fc7476b4a5cc2e66223bcb
Atul Prakash <aprakash@umich.edu>
2012-12-03 19:26:35 -0500

    updates to Classes_notes

--- Lecture_Code/Classes_Notes/Person/version1/myperson.cpp
+++ Lecture_Code/Classes_Notes/Person/version1/myperson.cpp
@@ -9,23 +9,40 @@ typedef struct {
 */
 
 class Person {
-public:
+private:
     string name;
     int yearofbirth;
-
-
+    string address;
+public:
+    Person(string name, int birthyear) {
+        this->name = name;
+        this->yearofbirth = birthyear;
+    }
+    /* Overloaded constructor with a default year of birth */
+    Person(string name) {
+        this->name = name;
+        this->yearofbirth = 2012;
+    }
+    string getname() {
+        return this->name;
+    }
+    int getyearofbirth() {
+        return this->yearofbirth;
+    }
+    int getage(int currentyear) {
+        return currentyear - this->yearofbirth;
+    }
 };
-
-
 int main() {
-    Person p1;
-    Person p2;
-    p1.name = "John";
-    p1.yearofbirth = 1970;
-    p2.name = "Mary";
-    p2.yearofbirth = 1963;
-    cout << p1.name << " is " << 2012-p1.yearofbirth << " years old \n";
-    cout << p2.name << " is " << 2012-p2.yearofbirth << " years old \n";
+    Person p1("John", 1970);
+    Person p2("Mitt");
+    p1.setaddress("1301 Beal Ave, Ann Arbor, MI 48109");
+    cout << p1.getaddress() << endl;
+    // p1.setname("John");
+    cout << "P1's name is " << p1.getname() << endl;
+    cout << "P2's name is " << p2.getname() << endl;
+    cout << p2.getyearofbirth() << endl; // Access to private data. Will fail.
+    cout << p2.getname() << " age is " << p2.getage(2012) << endl; // Access to private data. Will fail.
 }
 
 
