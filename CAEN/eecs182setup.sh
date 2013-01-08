e6ec8935c2045cf30516deccbed0bbc4e147eff8
aprakashumich <aprakash@umich.edu>
2013-01-08 03:17:42 -0500

    updates

--- CAEN/eecs182setup.sh
+++ CAEN/eecs182setup.sh
@@ -3,7 +3,7 @@ mkdir ~/eecs182
 fs setacl ~/eecs182 $USER all -clear
 cd ~/eecs182
 ln -s ~aprakash/Public/eecs182/System/README.xt .
-git pull https://github.com/aprakashumich/hw_182w13 Homeworks
+git clone https://github.com/aprakashumich/hw_182w13 Homeworks
 echo "Your homework files are in eecs182/Homeworks folder. Follow the instructions in README.txt there"
 
 
