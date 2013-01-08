5612852f4a109977474d0b8a7f7d5317e2a59dbe
182 student <unknown>
2013-01-08 03:05:18 -0500

    updates

--- CAEN/eecs182setup.sh
+++ CAEN/eecs182setup.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-mkdir eecs182
-fs setacl eecs182 $USER all -clear
-cd eecs182
+mkdir ~/eecs182
+fs setacl ~/eecs182 $USER all -clear
+cd ~/eecs182
+ln -s ~aprakash/Public/eecs182/System/README.xt .
 git pull https://github.com/aprakashumich/hw_182w13 Homeworks
 echo "Your homework files are in eecs182/Homeworks folder. Follow the instructions in README.txt there"
 
